e13944a546e3b571851337e2112d7418108595c5
Joseph Manley <j@manleyweb.com>
2018-11-30 22:00:44 -0500

    Hostage Data Object
    Holds information for the "collectables"

---
+++ Assets/Scripts/Data Types/Hostage.cs
@@ -0,0 +1,16 @@
+
+
+
+//Gameplay actions, additional will be added for any game mechanic changes
+public enum HostageType
+{
+    Normal
+};
+
+//Collectable hostage information
+public struct Hostage {
+    public string name;
+    public string background;
+    public HostageType type;
+
+}
