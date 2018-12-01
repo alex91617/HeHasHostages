a732b40101ccf9d606f46141ee6ac5c5523b1315
Joseph Manley <j@manleyweb.com>
2018-11-30 23:03:39 -0500

    Updated Hostages and Added Collectables
    Hostages now have associated sprites that are dynamicly load and hostages are dynamicly loaded and stored in the 'Collectables' class. The 'Collectables' class can also grab a random hostage.

---
+++ Assets/Scripts/Data Types/Collectables.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Collectables {
+
+    public static List<Hostage> AllHostages;
+    public static List<Hostage> UnlockedHostages;
+	
+    public static void LoadCollectables()
+    {
+        AllHostages = new List<Hostage>();
+        UnlockedHostages = new List<Hostage>();
+        
+        //Grab all hostage file
+        var hostages = Resources.LoadAll<TextAsset>("Hostages");
+
+        //Convert text -> hostage and store data
+        foreach(TextAsset hostage in hostages)
+        {
+            Hostage obj = JsonUtility.FromJson<Hostage>(hostage.text);
+            AllHostages.Add(obj);
+            Debug.Log("Loading - hostage:" + obj.id);
+        }
+    }
+
+    static void SaveCollectables()
+    {
+    }
+
+
+
+    //Returns a random hostage or attempt to find a unique hostage randomly
+    static Hostage GrabAHostage(bool OnlyLocked = false)
+    {
+        if(OnlyLocked)
+        {
+            //Generate an empty hostage
+            Hostage hostage = new Hostage();
+
+            //Attempt to find a unique hostage (semi-randomly)
+            for(int i = 0; i < AllHostages.Count*2; i++)
+            {
+                bool IsUnique = true;
+                Hostage possibleHostage = AllHostages[Random.Range(0, AllHostages.Count - 1)];
+                foreach(Hostage AHostage in AllHostages)
+                {
+                    if(AHostage.id == possibleHostage.id)
+                    {
+                        IsUnique = false;
+                    }
+                }
+                //Return hostage if unique
+                if(IsUnique)
+                {
+                    hostage = possibleHostage;
+                    break;
+                }
+            }
+            //Return results
+            return hostage;
+        }
+        else
+        {
+            //Returns a random hostage
+            return AllHostages[Random.Range(0, AllHostages.Count - 1)];
+        }
+    }
+}
--- Assets/Scripts/Data Types/Hostage.cs
+++ Assets/Scripts/Data Types/Hostage.cs
@@ -1,16 +1,36 @@
-
-
-
+using UnityEngine;
 //Gameplay actions, additional will be added for any game mechanic changes
 public enum HostageType
 {
-    Normal
+    NORMAL
 };
 
 //Collectable hostage information
-public struct Hostage {
+public class Hostage {
+    //Variables
+    public string id;
     public string name;
     public string background;
+    private string sprite;
     public HostageType type;
 
+
+    public Sprite getSprite()
+    {
+        try
+        {
+            return Resources.Load<Sprite>("Art/Hostages/" + sprite + ".png");
+        }
+        catch
+        {
+            return Resources.Load<Sprite>("Art/error.png");
+        }
+    }
+
+    public Hostage()
+    {}
+    public Hostage(string spr)
+    {
+        this.sprite = spr;
+    }
 }
