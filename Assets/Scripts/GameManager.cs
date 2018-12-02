ba680950b7507a2e312cc1776fe07ff9563b7ead
Joseph Manley <j@manleyweb.com>
2018-12-01 20:18:05 -0500

    Implemented backstories

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
     Hostage nextHostage;
+
+    public GameObject hostageInfoDisplay;
     public Hostage currentHostage;
     public Transform NextHostageDisplay;
 	// Use this for initialization
@@ -17,10 +19,23 @@ public class GameManager : MonoBehaviour {
 	void Update () {
         NextHostageDisplay.Find("Text").GetComponent<Text>().text = nextHostage.name;
         NextHostageDisplay.Find("Image").GetComponent<Image>().sprite = nextHostage.getSprite();
-	}
+        hostageInfoDisplay.transform.Find("Name").GetComponent<Text>().text = nextHostage.name;
+        hostageInfoDisplay.transform.Find("Image").GetComponent<Image>().sprite = nextHostage.getSprite();
+        hostageInfoDisplay.transform.Find("Backstory").GetComponent<Text>().text = nextHostage.background;
+    }
     public void LoadHostage()
     {
         currentHostage = nextHostage;
         nextHostage = Collectables.GrabAHostage();
+    }
+
+    public void OpenHostageDisplay()
+    {
+        hostageInfoDisplay.SetActive(true);
+    }
+
+    public void CloseHostageDisplay()
+    {
+        hostageInfoDisplay.SetActive(false);
     }
 }
